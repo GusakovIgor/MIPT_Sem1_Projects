653f030df7520f3e8b1f05190094c5f900ad26cb
GusakovIgor <70455405+GusakovIgor@users.noreply.github.com>
2020-10-13 00:48:39 +0300

    Broken Stack

--- MyStack.h
+++ MyStack.h
@@ -19,88 +19,13 @@ typedef unsigned long long can_type;
 
 #define MIN_CAPACITY 1
 
-#define StackDump(stk, func)                                                                                              \
-        FILE* Logs = fopen("Logs.txt", "a");                                                                              \
-        int er_code = StackOK (stk);                                                                                      \
-        if (er_code != 1)                                                                                                 \
-        {                                                                                                                 \
-          fprintf(Logs, "ERROR in function: %s\n", #func);                                                                \
-          fprintf(Logs, "ERROR with code: %d\n", er_code);                                                                \
-          switch (er_code)                                                                                                \
-          {                                                                                                               \
-            case (-7):  fprintf(Logs, "HASH_PROTECTION_ERROR: Array has changed, but it shouldn't\n");  \
-                        break;                                                                                            \
-            case (-6):  fprintf(Logs, "CANARY_STACK_PROTECTION_ERROR: Elements outside stack structure are changing\n");  \
-                        break;                                                                                            \
-            case (-5):  fprintf(Logs, "CANARY_ARRAY_PROTECTION_ERROR: Elements outside array are changing\n");            \
-                        break;                                                                                            \
-            case (-4):  fprintf(Logs, "CONSTRUCTION_ERROR: Pointer on stk is NULL\n");                                    \
-                        break;                                                                                            \
-            case (-3):  fprintf(Logs, "MEMORY_ERROR: Pointer on array is NULL\n");                                        \
-                        break;                                                                                            \
-            case (-2):  fprintf(Logs, "SIZE_ERROR: Capacity < size, or size < 0. Probably expansion works incorrect\n");  \
-                        break;                                                                                            \
-            case (-1):  fprintf(Logs, "CHECK_IF_EMPTY_ERROR: Probably you put assert before checking if stack empty or something wrong with element stk->array[0]\n"); \
-                        break;                                                                                            \
-            case ( 0):  fprintf(Logs, "FILLING_ERROR: Some value in stk is NAN when it shouldn't be, or it's not NAN when should be\n"); \
-                        break;                                                                                            \
-          }                                                                                                               \
-        }                                                                                                                 \
-        fprintf(Logs, "\nTemporary Hash = %lld", stk->hsh->temp_value);                                                   \
-        fprintf(Logs, "\nHash = %lld", stk->hsh->value);                                                                  \
-        fprintf(Logs, "\nCanary_2 Left = %X", *CANARY_2L);                                                                    \
-        fprintf(Logs, "\n%s ", #stk);                                                                                     \
-        if (er_code != 1)                                                                                                 \
-        {                                                                                                                 \
-          fprintf(Logs, "(NOT OK) ");                                                                                     \
-        }                                                                                                                 \
-        else                                                                                                              \
-        {                                                                                                                 \
-          fprintf(Logs, "(OK) ");                                                                                         \
-        }                                                                                                                 \
-        fprintf(Logs, "[%p]", stk);                                                                                       \
-        fprintf(Logs, "\nsize = %d\n",   (stk)->size);                                                                    \
-        fprintf(Logs, "capacity = %d\n", (stk)->capacity);                                                                \
-        fprintf(Logs, "\nCanary_1 Left = %X\n", *CANARY_1L );                            \
-        fprintf(Logs, "array [%p]\n", stk->array);                                                                        \
-        fprintf(Logs, "      {\n");                                                                                       \
-        for (int i = 0; i < (stk)->capacity; i++)                                                                         \
-        {                                                                                                                 \
-            fprintf(Logs, "    ");                                                                                        \
-            if (isnan((stk)->array[i]))                                                                                   \
-            {                                                                                                             \
-                fprintf(Logs, "* ");                                                                                      \
-            }                                                                                                             \
-            else                                                                                                          \
-            {                                                                                                             \
-                fprintf(Logs, "  ");                                                                                      \
-            }                                                                                                             \
-            fprintf(Logs, "[%d] = %lg\n", i, (stk)->array[i]);                                                            \
-        }                                                                                                                 \
-        fprintf(Logs, "      }\n");                                                                                       \
-        fprintf(Logs, "Canary_1 Right = %X\n", *CANARY_1R );             \
-        fprintf(Logs, "\nCanary_2 Right = %X", *CANARY_2R);                                                                    \
-        fprintf(Logs, "\n\n\n\n\n");                                                                                      \
-        fclose (Logs);                                                                                                    \
- 
- 
-#define ASSERT_OK(stk, func)      \
-        if (StackOK(stk) < 1)     \
-        {                         \
-          printf ("\nYou can see, what's wrong in file \"Logs.txt\"\n\n");  \
-          StackDump(stk,func)    \
-          assert(!"OK");          \
-        }                         \
- 
 struct MyStack;
 struct MyHash;
 
 int StackOK (MyStack* stk);
+void ASSERT_OK(MyStack* stk, char* func);
+void StackDump(MyStack* stk, char* func);
 void LogsClear ();
-
-MyHash* Hash_Construct();
-void HashMaker (MyStack* stk, const char* check_flag);
-bool HashChecker (MyStack* stk);
 
 void Unit_Tests (int argc, const char* argv[]);
 void Help_Print ();
@@ -122,15 +47,9 @@ void StackPush  (MyStack* stk, double elem);
 double StackPop (MyStack* stk);
 double StackTop (MyStack* stk);
 
-struct MyHash
-{
-    long long int temp_value;
-    long long int value;
-};
-
 struct MyStack
 {
-    MyHash* hsh;
+    long long int hash;
     bool is_empty;
     size_t size;
     size_t capacity;
